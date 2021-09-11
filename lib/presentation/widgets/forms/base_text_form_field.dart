7a66dfa1cb9aa5fca361f8135e6be08534fa6816
Sulejman Limaj <limaj.sulejman@gmail.com>
2021-09-11 12:11:31 +0200

    TextFormField added

--- lib/presentation/widgets/forms/base_text_form_field.dart
+++ lib/presentation/widgets/forms/base_text_form_field.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class BaseTextFormField extends StatelessWidget {
-  const BaseTextFormField({ Key? key }) : super(key: key);
+  const BaseTextFormField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("text"),);
+    return TextFormField(
+      
+    );
   }
 }
