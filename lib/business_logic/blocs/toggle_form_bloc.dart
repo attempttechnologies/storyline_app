9827907c13628f53882ccadf6ca989630950bd8d
Sulejman Limaj <limaj.sulejman@gmail.com>
2021-09-14 08:13:14 +0200

    Toggle Bloc with Abstraction of Bloc

--- lib/business_logic/blocs/toggle_form_bloc.dart
+++ lib/business_logic/blocs/toggle_form_bloc.dart
@@ -1,10 +1,9 @@
 import 'package:storyline_app/business_logic/blocs/bloc.dart';
 
 class ToggleFormBloc implements Bloc {
-
   @override
   void dispose() {
     // TODO: implement dispose
-    
+
   }
 }
